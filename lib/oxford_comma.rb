951acac28ade17d733e9c37201b8c3bbed20dcaf
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-18 06:41:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
  if array.length < 2
    array.join
- elsif array.length == 2
-   array.insert(1, "and").join(" ")
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+   else
+     list = ", and " + array.pop
+     list = array.join(", ") << list
  end
 end
